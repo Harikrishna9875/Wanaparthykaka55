1e092601b28f4fa80f2a4314d42006abdeefecd3
Harikrishna9875 <harikrishnaravanaveni@gmail.com>
2026-01-24 13:26:09 +0530

    feat: restaurant + menu models + admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Restaurant, MenuCategory, MenuItem
 
-# Register your models here.
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ['name', 'owner', 'phone', 'is_open']
+    list_filter = ['is_open']
+
+@admin.register(MenuCategory)
+class MenuCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'restaurant']
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'is_available']
+    list_editable = ['is_available']
+    list_filter = ['is_veg', 'is_available']
