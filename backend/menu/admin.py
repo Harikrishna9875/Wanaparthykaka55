0726f8994c94ff06bd8dafed0811b3c0f59638ff
Harikrishna9875 <harikrishnaravanaveni@gmail.com>
2026-03-01 00:18:39 +0530

    Phase 1: Django backend initialized

--- backend/menu/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import MenuItem
-
-@admin.register(MenuItem)
-class MenuItemAdmin(admin.ModelAdmin):
-    list_display = ("name", "size", "price", "is_available")
-    list_filter = ("is_available", "size")
-    search_fields = ("name",)
